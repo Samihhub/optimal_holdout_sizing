d2ac65c020a0f8d6e18261b46a41d3bfe3da4486
Samihhub <47423350+Samihhub@users.noreply.github.com>
2021-06-16 09:22:01 +0100

    Add files via upload

--- sim_system.R
+++ sim_system.R
@@ -130,14 +130,14 @@ split_data <- function(X, frac) {
 ## Dynamics of the system
 # Initialisation of patient data
 boots <- 100      # Number of bootstrap resamplings
-nobs <- 10000                      # Number of observations, i.e patients
-nobs_boot <- 500         # Number of observations to resample in each bootstrap iter
-npreds <- 7                     # Number of predictors
+nobs <- 50000                      # Number of observations, i.e patients
+nobs_boot <- 50000         # Number of observations to resample in each bootstrap iter
+npreds <- 7                    # Number of predictors
 
 # Definition of holdout set sizes to test
 min_frac <- 0.002
 max_frac <- 0.02
-num_sizes <- 100
+num_sizes <- 150
 
 
 # Variables for the threshold estimation
@@ -145,25 +145,29 @@ c_tn <- 0 # Cost of true neg
 c_tp <- 0.5 # Cost of true pos
 c_fp <- 0.5 # Cost of false pos
 c_fn <- 1 # Cost of false neg
-num_probs <- 10 # Number of probabilities to scan
-prob_vals <- seq(0, 1, length.out = num_probs)
+num_thresh <- 10 # Number of probabilities to scan
+prob_vals <- seq(0, 1, length.out = num_thresh) # Vector with probability thresh to scan
+thresh_vals <- seq()
+k <- 5 # Number of CV folds
 
 
 # Fraction of patients assigned to the holdout set
 frac_ho <- seq(min_frac, max_frac, length.out = num_sizes)
 dyn_cols = colorRampPalette(c("red", "blue"))(num_sizes) # Colour palette
 
-costs <- matrix(nrow = boots, ncol = num_sizes)
+costs_boot <- matrix(nrow = boots, ncol = num_sizes)
+costs <- seq()
 
 
 # Matrix with the number of deaths per epoch and holdout size
-deaths_inter <- deaths_ho <- matrix(nrow = boots, ncol = num_sizes)
+deaths_boot_inter <- deaths_boot_ho <- matrix(nrow = boots, ncol = num_sizes)
+deaths_inter <- deaths_ho <- seq()
+
+
+# Initialise Progress Bar
+#if (exists(prog_bar)) rm(prog_bar)
+#prog_bar <- progress_bar$new()
 old_progress <- 0
-avg_disease <- matrix(nrow = boots, ncol = num_sizes)
-
-
-if (exists(prog_bar)) rm(prog_bar)
-prog_bar <- progress_bar$new()
 
 
 # Initialise Dataset
@@ -174,129 +178,174 @@ Y <- newdata$classes
 coefs <- newdata$coefs
 
 for (i in 1:num_sizes) {
-  for (b in 1:boots) {
-    set.seed(b + i*boots)    # delete?
-    thresh <- 0.5 # Decision boundary
+#  thresh <- 0.5
+#  pat_data <- cbind(X, Y)
+#  pat_data["Y"] <- lapply(pat_data["Y"], factor)
+#  
+#  mask <- split_data(pat_data, frac_ho[i])
+#  data_interv <- pat_data[!mask,]
+#  data_hold <- pat_data[mask,]
+#  
+#  # Calculate optimal threshold
+#  indices <- sample(1:nrow(data_hold))
+#  folds <- cut(1:length(indices), breaks = k, labels = FALSE) # Mask to partition data in CV analysis
+#  cost_tot <- numeric(num_thresh)
+#  
+#  for (f in 1:k) {
+#    # Train model for each fold of CV, then scan all probs to find loss
+#    val_indices <- folds == f
+#    val_data <- data_hold[val_indices,]
+#    partial_train_data <- data_hold[!val_indices,]
+#    
+#    thresh_model <- glm(Y ~ ., data = partial_train_data, 
+#                     family = binomial(link = "logit"))
+#    
+#    thresh_pred <- predict(thresh_model, newdata = val_data, 
+#                           type = "response")
+#    
+#    for(p in 1:num_thresh){
+#      num_tn <- sum(as.numeric(val_data["Y"] == 0) & as.numeric(thresh_pred < prob_vals[p]))
+#      num_fn <- sum(as.numeric(val_data["Y"] == 1) & as.numeric(thresh_pred < prob_vals[p]))
+#      num_fp <- sum(as.numeric(val_data["Y"] == 0) & as.numeric(thresh_pred >= prob_vals[p]))
+#      num_tp <- sum(as.numeric(val_data["Y"] == 1) & as.numeric(thresh_pred >= prob_vals[p]))
+#      
+#      cost_tot[p] <- cost_tot[p] + c_tn * num_tn + 
+#        c_tp * num_tp + 
+#        c_fn * num_fn + 
+#        c_fp * num_fp
+#    }
+#  }
+#  
+#  # Rescale loss
+#  cost_tot <- cost_tot / k
+#  
+#  # Minimise loss to find classif threshold
+#  thresh <- prob_vals[which.min(cost_tot)]
+#  
+#  # Predict
+#  glm_model <- glm(Y ~ ., data = data_hold, 
+#                   family = binomial(link = "logit"))
+#  glm_pred <- predict(glm_model, newdata = data_interv, type = "response")
+#  class_pred <- ifelse(glm_pred > thresh, '1', '0')
+#  
+#  # Dr is an oracle for the first predictor and blind for the rest of them
+#  dr_pred <- oracle_pred(data_hold, coefs)
+#  
+#  
+#  # Those with disease, predicted not to die, will die
+#  deaths_inter[i] <- sum(data_interv$Y == 1 & class_pred != 1)
+#  deaths_ho[i] <- sum(data_hold$Y == 1 & dr_pred != 1)
+  
+  # Bootstrapping loop, the iteration 0 uses the whole dataset to calculate the
+  # estimates of the predictions.
+  
+  for (b in 0:boots) {
+    progress <- 100 * (((i - 1) * boots) + b) / (num_sizes * (boots + 1))
     
-    # Resample Patient data
-    samp_mask <- sample(1:nobs, nobs_boot, replace = TRUE) # mask to resample data
-    pat_data <- cbind(X, Y)[samp_mask,]
-    pat_data["Y"] <- lapply(pat_data["Y"], factor)
-    
-    avg_disease[b, i] <- sum(Y)
-    
-    progress <- 100 * (((i - 1) * boots) + b - 1) / (num_sizes * boots)
     if (abs(floor(old_progress) - floor(progress)) > 0) {
       cat(floor(progress), "%\n")
       #for(aux in 1:abs(floor(old_progress) - floor(progress))) prog_bar$tick()
     }
+    
+    set.seed(b + i*boots)
+    thresh <- 0.5 # Decision boundary
+    
+    # Resample Patient data
+    if (b) {
+      samp_mask <- sample(1:nobs, nobs_boot, replace = TRUE) # mask to resample data
+      pat_data <- cbind(X, Y)[samp_mask,]
+    } else pat_data <- cbind(X, Y)
+    
+    pat_data["Y"] <- lapply(pat_data["Y"], factor)
     
     # For each holdout size, split data into intervention and holdout set
     mask <- split_data(pat_data, frac_ho[i])
     data_interv <- pat_data[!mask,]
     data_hold <- pat_data[mask,]
     
-    # Holdout set mlr3 task
-    hold_task <- TaskClassif$new(id = "Holdout",
-                                 backend = data_hold,
-                                 target = "Y",
-                                 positive = '1')
-    
-    # Intervention set mlr3 task
-    interv_task <- TaskClassif$new(id = "Intervention",
-                                   backend = data_interv,
-                                   target = "Y",
-                                   positive = '1')
-    
-    # Initialise logreg Learner
-    lrn_patient  <- lrn("classif.log_reg", predict_type = "prob")
-    
     # Calculate optimal threshold
-    k <- 5 # Number of CV folds
     indices <- sample(1:nrow(data_hold))
     folds <- cut(1:length(indices), breaks = k, labels = FALSE) # Mask to partition data in CV analysis
-    cost_tot <- numeric(num_probs)
+    cost_tot <- numeric(num_thresh)
     
-    for(p in 1:num_probs){
-      for (f in 1:k) {
-        val_indices <- folds == f
-        val_data <- data_hold[val_indices,]
-        partial_train_data <- data_hold[!val_indices,]
+    for (f in 1:k) {
+      # Train model for each fold of CV, then scan all probs to find loss
+      val_indices <- folds == f
+      val_data <- data_hold[val_indices,]
+      partial_train_data <- data_hold[!val_indices,]
+      
+      thresh_model <- glm(Y ~ ., data = partial_train_data, 
+                          family = binomial(link = "logit"))
+      
+      thresh_pred <- predict(thresh_model, newdata = val_data, 
+                             type = "response")
+      
+      for(p in 1:num_thresh){
+        num_tn <- sum(as.numeric(val_data["Y"] == 0) & as.numeric(thresh_pred < prob_vals[p]))
+        num_fn <- sum(as.numeric(val_data["Y"] == 1) & as.numeric(thresh_pred < prob_vals[p]))
+        num_fp <- sum(as.numeric(val_data["Y"] == 0) & as.numeric(thresh_pred >= prob_vals[p]))
+        num_tp <- sum(as.numeric(val_data["Y"] == 1) & as.numeric(thresh_pred >= prob_vals[p]))
         
-        partial_task <- TaskClassif$new(id = "CV_ho_train",
-                                        backend = partial_train_data,
-                                        target = "Y",
-                                        positive = '1')
-        
-        partial_val_task <- TaskClassif$new(id = "CV_ho_val",
-                                            backend = val_data,
-                                            target = "Y",
-                                            positive = '1')
-        
-        lrn_patient$train(partial_task)
-        model_thresh <- lrn_patient$predict(partial_val_task)
-        
-        model_thresh$set_threshold(prob_vals[p])
-        num_tp <- model_thresh$confusion[1]
-        num_fn <- model_thresh$confusion[2]
-        num_fp <- model_thresh$confusion[3]
-        num_tn <- model_thresh$confusion[4]
         cost_tot[p] <- cost_tot[p] + c_tn * num_tn + 
           c_tp * num_tp + 
           c_fn * num_fn + 
           c_fp * num_fp
       }
-      cost_tot[p] <- cost_tot[p] / k
     }
     
+    # Rescale loss
+    cost_tot <- cost_tot / k
+    
     # Train model
-    lrn_patient$train(hold_task)
+    glm_model <- glm(Y ~ ., data = data_hold, 
+                     family = binomial(link = "logit"))
     thresh <- prob_vals[which.min(cost_tot)]
-    model_thresh$set_threshold(thresh)
-    costs[b, i] <- min(cost_tot)
+    if (b) costs_boot[b, i] <- min(cost_tot) else costs <- min(cost_tot)
     
     # Predict
-    model_pred <- lrn_patient$predict(interv_task)
-    model_resp <- model_pred$response
-    
+    glm_pred <- predict(glm_model, newdata = data_interv, type = "response")
+    class_pred <- ifelse(glm_pred > thresh, '1', '0')
     
     # Dr is an oracle for the first predictor and blind for the rest of them
     dr_pred <- oracle_pred(data_hold, coefs)
     
-    
     # Those with disease, predicted not to die, will die
-    deaths_inter[b, i] <- sum(data_interv$Y == 1 & model_resp != 1)
-    deaths_ho[b, i] <- sum(data_hold$Y == 1 & dr_pred != 1)
+    if (b){
+      deaths_boot_inter[b, i] <- sum(data_interv$Y == 1 & class_pred != 1)
+      deaths_boot_ho[b, i] <- sum(data_hold$Y == 1 & dr_pred != 1)
+    } else {
+      deaths_inter[i] <- sum(data_interv$Y == 1 & class_pred != 1)
+      deaths_ho[i] <- sum(data_hold$Y == 1 & dr_pred != 1)
+    }
     
     old_progress <- progress
   }
 }
 
 deaths_per_frac <- deaths_ho + deaths_inter
-deaths_sd <- apply(deaths_per_frac, 2, sd)
+deaths_boot_tot <- deaths_boot_ho + deaths_boot_inter
+deaths_sd <- apply(deaths_boot_tot, 2, sd)
 
-avg_deaths <- colSums(deaths_per_frac) / boots
-
-plot(frac_ho, avg_deaths,
+plot(frac_ho, deaths_per_frac,
 #plot(costs, deaths_inter,
      pch = 16,
      ylab = "Deaths",
      xlab = "Holdout set size",
-     ylim = c(min(avg_deaths - deaths_sd), max(avg_deaths + deaths_sd)))
-arrows(frac_ho, 
-       avg_deaths - deaths_sd,
-       frac_ho,
-       avg_deaths + deaths_sd, 
-       length=0.05, angle=90, code=3)
-#points(frac_ho, deaths_inter, col = 2, pch = 16)
-#points(frac_ho, deaths_ho, col = 3, pch = 16)
-#points(frac_ho, avg_disease, col = 4, pch = 16)
+     #ylim = c(min(deaths_per_frac - deaths_sd), max(deaths_per_frac + deaths_sd)))
+    ylim = c(0, max(deaths_per_frac + deaths_sd)))
 
+points(frac_ho, deaths_inter, pch = 16, col = 2)
+
+arrows(frac_ho, 
+       deaths_per_frac - deaths_sd,
+       frac_ho,
+       deaths_per_frac + deaths_sd, 
+       length=0.05, angle=90, code=3)
 
 
 ##### Snippet for hist of probs
 set.seed(100)  
-npreds = 10
+npreds = 7
 X <- gen_preds(nobs, npreds)
 probs_hist <- gen_resp(X, retprobs = TRUE, coefs_sd = 6)
 hist(probs_hist)
