172843dac12b438574e54fcf4ff6e063923adf37
Sami <sami.haidar-wehbe@durham.ac.uk>
2021-06-22 14:29:30 +0100

    Improved section structure Deleted unnecesary functions Implemented changes suggested by James: Flag for threshold estimation, generalisation of cost function, Plot averages of bootstrap instead of point estimates.

--- sim_system.R
+++ sim_system.R
@@ -1,9 +1,11 @@
-################################################################################
-## Set seed for reproducibility
-set.seed(1234)
+#### Simulation ####
+## Simulation for the estimation of the optimal holdout set size ##
+## for intervention on a population, guided by a risk score      ##
 
-################################################################################
-## Call libraries
+# Ctrl+Alt+T to run section by section
+
+# ---------------------------------------------------------------------------- #
+#### Package loading ####
 library("mlr3pipelines")
 library("mlr3tuning")
 library("mlr3filters")
@@ -11,52 +13,16 @@ library("mlr3verse")
 library("dplyr")
 library("progress")
 
-################################################################################
+
+
+# ---------------------------------------------------------------------------- #
+#### Function Definitions ####
+
 ## Model and Mathematical function Definitions
 logistic <- function(x) 1/(1 + exp(-x))
 logit <- function(x) -log(1/x - 1)
 
-
-# Risk score before intervention, given rho and covariates before intervention
-f <- function(xs, xa) {
-  logistic(xs + xa)
-}
-
-
-# Risk score after intervention, given rho and covariates before intervention
-h <- function(xs, xa, r) f(xs, g(r, xa))
-
-
-# Intervention function, taking X(0) -> X(1)
-g <- function(rho, xa) {
-  (xa + 0.5 * (xa + sqrt(1 + xa^2)))*(1 - rho) + 
-    (xa - 0.5 * (xa + sqrt(1 + xa^2))) * rho
-}
-
-
-# Risk score at e calculated from the value at e-1
-rho_update <- function(xs, xa, e, old_rho){
-  if (e == 0) {
-    return(f(xs, xa))
-  }
-  
-  return(h(xs, xa, old_rho))
-}
-
-
-drift <- function(X, coefs) {
-  # For now, implemented drift for the model, changing the coefficients of the
-  # logreg model that calculates the true classes.
-  nobs <- dim(X)[1]
-  npreds <- dim(X)[2]
-  #X <- X + matrix(rnorm(nobs * npreds) / 10, nobs, npreds)
-  coefs <- coefs + rnorm(npreds) / 10
-  newdata <- gen_resp(X, coefs)
-  
-  return(list("X" = X, "Y" = newdata$classes, "coefs" = newdata$coefs))
-}
-
-
+# Estimate Dr's predictions
 oracle_pred <- function(X, coefs){
   #Dr is oracle to the first predictor and blind to the other ones.
   X <- X %>% select(1, 2)
@@ -73,11 +39,6 @@ oracle_pred <- function(X, coefs){
   return(rbinom(nobs, 1, probs))
 }
 
-
-
-
-################################################################################
-## Function definitions
 
 # Generate the predictors matrix
 gen_preds <- function(nobs, npreds) {
@@ -127,18 +88,22 @@ split_data <- function(X, frac) {
 
 
 
-################################################################################
-## Dynamics of the system
+# -----------------------------#
+#### Dynamics of the system ####
+
+## Set seed for reproducibility
+set.seed(1234)
+
 # Initialisation of patient data
 boots <- 100      # Number of bootstrap resamplings
-nobs <- 50000                      # Number of observations, i.e patients
-nobs_boot <- 50000         # Number of observations to resample in each bootstrap iter
+nobs <- 5000                      # Number of observations, i.e patients
+nobs_boot <- 5000         # Number of observations to resample in each bootstrap iter
 npreds <- 7                    # Number of predictors
 
 # Definition of holdout set sizes to test
-min_frac <- 0.002
-max_frac <- 0.02
-num_sizes <- 150
+min_frac <- 0.02
+max_frac <- 0.2
+num_sizes <- 50
 
 
 # Variables for the threshold estimation
@@ -150,14 +115,19 @@ num_thresh <- 10 # Number of probabilities to scan
 prob_vals <- seq(0, 1, length.out = num_thresh) # Vector with probability thresh to scan
 thresh_vals <- seq()
 k <- 5 # Number of CV folds
+set_thresh <- TRUE # Flag to estimate classification threshold
 
 
 # Fraction of patients assigned to the holdout set
 frac_ho <- seq(min_frac, max_frac, length.out = num_sizes)
 dyn_cols = colorRampPalette(c("red", "blue"))(num_sizes) # Colour palette
 
+
+# Cost estimation
 costs_boot <- matrix(nrow = boots, ncol = num_sizes)
 costs <- seq()
+cost_type <- "gen_cost" # gen_cost = generalised cost, deaths = deaths (i.e. fn)
+cost_mat <- rbind(c(c_tn, c_fp), c(c_fn, c_tp))
 
 
 # Matrix with the number of deaths per epoch and holdout size
@@ -179,75 +149,9 @@ Y <- newdata$classes
 coefs <- newdata$coefs
 
 
-######### CHECK THIS
-#mask=mask[order(runif(length(mask)))]
-#mask = sample(mask) ?? sample permutes if no other params
-
-
-
 
 for (i in 1:num_sizes) {
-  #  thresh <- 0.5
-  #  pat_data <- cbind(X, Y)
-  #  pat_data["Y"] <- lapply(pat_data["Y"], factor)
-  #  
-  #  mask <- split_data(pat_data, frac_ho[i])
-  #  data_interv <- pat_data[!mask,]
-  #  data_hold <- pat_data[mask,]
-  #  
-  #  # Calculate optimal threshold
-  #  indices <- sample(1:nrow(data_hold))
-  #  folds <- cut(1:length(indices), breaks = k, labels = FALSE) # Mask to partition data in CV analysis
-  #  cost_tot <- numeric(num_thresh)
-  #  
-  #  for (f in 1:k) {
-  #    # Train model for each fold of CV, then scan all probs to find loss
-  #    val_indices <- folds == f
-  #    val_data <- data_hold[val_indices,]
-  #    partial_train_data <- data_hold[!val_indices,]
-  #    
-  #    thresh_model <- glm(Y ~ ., data = partial_train_data, 
-  #                     family = binomial(link = "logit"))
-  #    
-  #    thresh_pred <- predict(thresh_model, newdata = val_data, 
-  #                           type = "response")
-  #    
-  #    for(p in 1:num_thresh){
-  #      num_tn <- sum(as.numeric(val_data["Y"] == 0) & as.numeric(thresh_pred < prob_vals[p]))
-  #      num_fn <- sum(as.numeric(val_data["Y"] == 1) & as.numeric(thresh_pred < prob_vals[p]))
-  #      num_fp <- sum(as.numeric(val_data["Y"] == 0) & as.numeric(thresh_pred >= prob_vals[p]))
-  #      num_tp <- sum(as.numeric(val_data["Y"] == 1) & as.numeric(thresh_pred >= prob_vals[p]))
-  #      
-  #      cost_tot[p] <- cost_tot[p] + c_tn * num_tn + 
-  #        c_tp * num_tp + 
-  #        c_fn * num_fn + 
-  #        c_fp * num_fp
-  #    }
-  #  }
-  #  
-  #  # Rescale loss
-  #  cost_tot <- cost_tot / k
-  #  
-  #  # Minimise loss to find classif threshold
-  #  thresh <- prob_vals[which.min(cost_tot)]
-  #  
-  #  # Predict
-  #  glm_model <- glm(Y ~ ., data = data_hold, 
-  #                   family = binomial(link = "logit"))
-  #  glm_pred <- predict(glm_model, newdata = data_interv, type = "response")
-  #  class_pred <- ifelse(glm_pred > thresh, '1', '0')
-  #  
-  #  # Dr is an oracle for the first predictor and blind for the rest of them
-  #  dr_pred <- oracle_pred(data_hold, coefs)
-  #  
-  #  
-  #  # Those with disease, predicted not to die, will die
-  #  deaths_inter[i] <- sum(data_interv$Y == 1 & class_pred != 1)
-  #  deaths_ho[i] <- sum(data_hold$Y == 1 & dr_pred != 1)
-  
-  # Bootstrapping loop, the iteration 0 uses the whole dataset to calculate the
-  # estimates of the predictions.
-  
+  # b=0 is the point estimate, b>0 are bootstrap samples
   for (b in 0:boots) {
     progress <- 100 * (((i - 1) * boots) + b) / (num_sizes * (boots + 1))
     
@@ -305,10 +209,13 @@ for (i in 1:num_sizes) {
     # Rescale loss
     cost_tot <- cost_tot / k
     
+    
+    # THIS NEEDS REWRITTING!!!!!!!!!
+    # CURRENT COSTS ARE CALCULATED WITH THRESHOLD ESTIMATION, EVEN WHEN FLAG IS OFF
     # Train model
     glm_model <- glm(Y ~ ., data = data_hold, 
                      family = binomial(link = "logit"))
-    thresh <- prob_vals[which.min(cost_tot)]
+    thresh <- ifelse(set_thresh, prob_vals[which.min(cost_tot)], 0.5)
     if (b) costs_boot[b, i] <- min(cost_tot) else costs <- min(cost_tot)
     
     # Predict
@@ -318,13 +225,39 @@ for (i in 1:num_sizes) {
     # Dr is an oracle for the first predictor and blind for the rest of them
     dr_pred <- oracle_pred(data_hold, coefs)
     
+    
     # Those with disease, predicted not to die, will die
-    if (b){
-      deaths_boot_inter[b, i] <- sum(data_interv$Y == 1 & class_pred != 1)
-      deaths_boot_ho[b, i] <- sum(data_hold$Y == 1 & dr_pred != 1)
-    } else {
-      deaths_inter[i] <- sum(data_interv$Y == 1 & class_pred != 1)
-      deaths_ho[i] <- sum(data_hold$Y == 1 & dr_pred != 1)
+    # This if clause calculates the cost for each h.o. set size 
+    # as the number of deaths for each of them
+    
+    if (cost_type == "deaths"){
+      if (b){
+        deaths_boot_inter[b, i] <- sum(data_interv$Y == 1 & class_pred != 1)
+        deaths_boot_ho[b, i] <- sum(data_hold$Y == 1 & dr_pred != 1)
+      } else {
+        deaths_inter[i] <- sum(data_interv$Y == 1 & class_pred != 1)
+        deaths_ho[i] <- sum(data_hold$Y == 1 & dr_pred != 1)
+      }
+    }
+    
+    
+    # Alternatively, use a generalised cost function with a specific cost for
+    # each of fn, fp, tp and tn
+    if (cost_type == "gen_cost"){
+      # Generate confusion matrices
+      confus_inter <- table(factor(data_interv$Y, levels=0:1), 
+                             factor(class_pred, levels=0:1))
+      
+      confus_hold <- table(factor(data_hold$Y, levels=0:1), 
+                           factor(dr_pred, levels=0:1))
+      
+      if (b){
+        deaths_boot_inter[b, i] <- sum(confus_inter * cost_mat)
+        deaths_boot_ho[b, i] <- sum(confus_hold * cost_mat)
+      } else {
+        deaths_inter[i] <- sum(confus_inter * cost_mat)
+        deaths_ho[i] <- sum(confus_hold * cost_mat)
+      }
     }
     
     old_progress <- progress
@@ -341,7 +274,7 @@ plot(frac_ho, deaths_per_frac,
      ylab = "Deaths",
      xlab = "Holdout set size",
      #ylim = c(min(deaths_per_frac - deaths_sd), max(deaths_per_frac + deaths_sd)))
-     ylim = c(0, max(deaths_per_frac + deaths_sd)))
+     ylim = c(min(deaths_inter), max(deaths_per_frac + deaths_sd)))
 
 points(frac_ho, deaths_inter, pch = 16, col = 2)
 
@@ -352,7 +285,34 @@ arrows(frac_ho,
        length=0.05, angle=90, code=3)
 
 
-##### Snippet for hist of probs
+#### Test James' code ####
+### QUESTIONS:
+# Why [1:77] in points?
+# 
+
+plot(frac_ho, deaths_per_frac,type="n", 
+     #plot(costs, deaths_inter,
+     pch = 16,
+     ylab = "Deaths",
+     xlab = "Holdout set size",
+     #ylim = c(min(deaths_per_frac - deaths_sd), max(deaths_per_frac + deaths_sd)))
+     ylim = range(c(deaths_per_frac - deaths_sd,deaths_per_frac + deaths_sd),na.rm=T))
+
+points(frac_ho, colMeans(deaths_boot_tot,na.rm=T), pch = 16, col = 2)
+
+q1=0.05; q2=0.95
+arrows(frac_ho, 
+       apply(deaths_boot_tot,2,function(x) quantile(x,q1,na.rm=T)), 
+       frac_ho,
+       apply(deaths_boot_tot,2,function(x) quantile(x,q2,na.rm=T)),
+       length=0.05, angle=90, code=3)
+
+
+
+
+#### ---- Debugging snippets ---- ####
+
+###### Snippet for hist of probs
 set.seed(100)  
 npreds = 7
 X <- gen_preds(nobs, npreds)
@@ -401,4 +361,4 @@ for(i in 1:num_probs){
     c_fp * num_fp * 4 
 }
 plot(prob_vals, cost_tot)
-cost_tot
+cost_to
